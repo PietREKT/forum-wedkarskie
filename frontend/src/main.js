5a255e49726c119e4b77474fb79a2c3d13f0f46c
PietREKT <piotr_stoinski02@wp.pl>
2025-10-08 12:12:44 +0200

    Initalized project

---
+++ frontend/src/main.js
@@ -0,0 +1,29 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from './resources/routes'
+import { createI18n } from "vue-i18n";
+import { defaultLocale, languages } from './resources/locales'
+
+
+const pinia = createPinia();
+const messages = Object.assign(languages)
+
+const i18n = createI18n({
+    locale: defaultLocale,
+    fallbackLocale: 'en',
+    messages
+})
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+createApp(App)
+    .use(pinia)
+    .use(router)
+    .use(i18n)
+.mount('#app')
---
+++ frontend/src/resources/locales/index.js
@@ -0,0 +1,8 @@
+import en from '@/resources/locales/en.json'
+import pl from '@/resources/locales/pl.json'
+export const defaultLocale = 'pl'
+
+export const languages = {
+    en: en,
+    pl: pl
+}
